28625f64cbf7cffcc84f2207069a5ef3bd7fa123
salbourhim4 <salbourhim4@gmail.com>
2025-12-23 13:01:07 -0500

    Add simulation for N walks

--- src/random_walk_1d.py
+++ src/random_walk_1d.py
@@ -1,8 +1,10 @@
 import random
+import numpy as np
 
 def random_walk(T): # A function that simulates a 1D random walk of a particle, T representing the number of steps
 
     x_particle = [0] #initially zero because particle starts at x = 0
+    # due to this initial value of zero, every array of random_walk will have length of T+1
 
     for steps in range(T):
         random_step = random.randint(1, 2) # 1 being left and 2 being right
@@ -14,9 +16,20 @@ def random_walk(T): # A function that simulates a 1D random walk of a particle, 
             x_particle.append(new_x_pos2)
     return x_particle
 
+def N_walks(N, T): #simulate N 1D random walks of T steps
 
-print("x position over 100 steps", random_walk(100))
-print("length of list:", len(random_walk(100)))
+    all_walks = np.empty((N, T+1), dtype = int) #empty 2D array of ints, N rows and T+1 columns
+
+    for i in range(N):
+        all_walks[i] = np.array(random_walk(T)) #adds the random_walk array to the 2D array
+    
+    return all_walks
+
+
+
+    
+# print("x position over 100 steps", random_walk(100))
+# print("length of list:", len(random_walk(100)))
 
 
 
